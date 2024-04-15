eb9d36c6e6942864a6368da373d8cd71510b1bb1
Santiago <santiagomendivil1009@gmail.com>
2024-04-15 15:15:02 -0600

    Added comments on the functionality of a binary tree

--- data-structures/binary_trees.cpp
+++ data-structures/binary_trees.cpp
@@ -1,5 +1,37 @@
+/*
+    The weight on a binary tree depends on the total nodes that exist in a level
+
+    Unorder: left, root, right
+    Pre-order: root, left, right
+    Post-order: left, right, root
+
+    Methods:
+        Add:
+            if tree is NULL
+                create new node
+                left_pointer = NULL
+                right_pointer = NULL
+
+            if value is less than the tree node (root)
+                add new node to the left
+
+            if value is bigger than the tree node (root)
+                add new node to the right
+
+        Show:
+            if tree is NULL
+                Show a message
+            else
+                Show the left tree first
+                Then show the right tree
+
+*/
 #include <iostream>
 using namespace std;
+
+struct nodo
+{
+};
 
 int main()
 {
